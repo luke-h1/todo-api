17937fc27360f9da68f3934f408814b0126fa461
luke-h1 <luke.howsam@yahoo.com>
2021-07-23 11:28:58 +0100

    setup db and routes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/luke-h1/todo-api
 
 go 1.16
 
-require github.com/gofiber/fiber v1.14.6 // indirect
+require (
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/luke-h1/todo-api/database"
+	"github.com/luke-h1/todo-api/todo"
 )
 
-func testHandler(c *fiber.Ctx) {
-	c.Send("Hello world 👋")
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/todo", todo.GetTodos)
+	app.Get("/api/todo/:id", todo.GetTodo)
+	app.Post("/api/todo", todo.NewTodo)
+	app.Delete("/api/todo/:id", todo.DeleteTodo)
+
+}
+
+func createConn() {
+	var err error
+	database.DBConn, err = gorm.Open("sqllites3", "todos.db")
+	if err != nil {
+		panic("❌ Failed to connect to DB!")
+	}
+	fmt.Println("✅ Succesfully connected to DB")
 }
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", testHandler)
+	createConn()
+
+	// close DB conns as soon as possible
+	defer database.DBConn.Close()
+
+	setupRoutes(app)
+
 	app.Listen(4000)
 }
